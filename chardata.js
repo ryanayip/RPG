73a0437df9f9946617e239240f4a57857de6f1b5
ryip94 <ryip94@yahoo.com>
2023-08-04 05:12:52 -0700

    Data management
    Updated chardata.js. Consolidated all character data into one object with nested objects. Updated references in main js file as well.

--- chardata.js
+++ chardata.js
@@ -1,19 +1,21 @@
 //data with character information
 
-export const hero = {
-  elementId: "hero",
-  name: "Hero",
-  avatar: "images/knight-blue.png",
-  health: 60,
-  diceRoll: [3, 1, 4],
-  diceCount: 3,
-};
+export const characterData = {
+  hero: {
+    elementId: "hero",
+    name: "Hero",
+    avatar: "images/knight-blue.png",
+    health: 60,
+    diceRoll: [3, 1, 4],
+    diceCount: 3,
+  },
 
-export const enemy = {
-  elementId: "enemy",
-  name: "Enemy",
-  avatar: "images/knight-red.png",
-  health: 10,
-  diceRoll: [2],
-  diceCount: 1,
+  enemy: {
+    elementId: "enemy",
+    name: "Enemy",
+    avatar: "images/knight-red.png",
+    health: 10,
+    diceRoll: [2],
+    diceCount: 1,
+  },
 };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { hero, enemy } from "./chardata.js";
+import { characterData } from "./chardata.js";
 
 //Character class
 class Character {
@@ -9,7 +9,7 @@ class Character {
   //method to render character to the DOM
   getCharacter() {
     const diceHtml = this.getDiceHTML(this.diceCount).join("");
-    document.getElementById(`${this.elementId}`).innerHTML = `
+    return `
           <div class="character-card">
               <h4 class="name">${this.name}</h4>
               <img class="avatar" src=${this.avatar} />
@@ -34,12 +34,16 @@ function getDiceRollArray(diceCount) {
   });
 }
 
-//Creating the two characters from the Character class
+//Creating two instances of the Character class for blue and red knight
 
-const blueKnight = new Character(hero);
-const redKnight = new Character(enemy);
+const blueKnight = new Character(characterData.hero);
+const redKnight = new Character(characterData.enemy);
 
-//Rendering two characters
+//Rendering two characters by setting the innerHTML of the DOM elements to the
+//return of the getCharacter() method
+function render() {
+  document.getElementById("hero").innerHTML = blueKnight.getCharacter();
+  document.getElementById("enemy").innerHTML = redKnight.getCharacter();
+}
 
-blueKnight.getCharacter();
-redKnight.getCharacter();
+render();
