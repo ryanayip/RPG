0870e80fef0fdc17f0aa7c6449c4d1c1f4bc23a7
ryip94 <ryip94@yahoo.com>
2023-08-04 01:54:55 -0700

    Constructor functions
    added a Character constructor function to replace hard coded functions. Added old renderChar function and refactored into a method of the Charactor constructor

--- index.js
+++ index.js
@@ -16,14 +16,37 @@ const enemy = {
   diceCount: 1,
 };
 
+//Character class
+
+function Character(data) {
+  this.elementId = data.elementId;
+  this.name = data.name;
+  this.avatar = data.avatar;
+  this.health = data.health;
+  this.diceRoll = data.diceRoll;
+  this.diceCount = data.diceCount;
+  //method to render character to the DOM
+  this.getCharacter = () => {
+    const diceHtml = getDiceHTML(this.diceCount).join("");
+    document.getElementById(`${this.elementId}`).innerHTML = `
+          <div class="character-card">
+              <h4 class="name">${this.name}</h4>
+              <img class="avatar" src=${this.avatar} />
+              <p class="health">health: <b> ${this.health} </b></p>
+              <div class="dice-container">${diceHtml}
+          </div>
+      `;
+  };
+}
+
+const blueKnight = new Character(hero);
+const redKnight = new Character(enemy);
 //Roll dice function
 
 function getDiceRollArray(diceCount) {
-  const newDiceRolls = [];
-  for (let i = 0; i < diceCount; i++) {
-    newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
-  }
-  return newDiceRolls;
+  return new Array(diceCount).fill(0).map(() => {
+    return Math.floor(Math.random() * 6) + 1;
+  });
 }
 
 function getDiceHTML(diceCount) {
@@ -32,22 +55,7 @@ function getDiceHTML(diceCount) {
   });
 }
 
-//Render character function
-function renderCharacter(character) {
-  const { elementId, name, avatar, health, diceRoll, diceCount } = character;
-
-  const diceHtml = getDiceHTML(diceCount).join("");
-
-  document.getElementById(`${elementId}`).innerHTML = `
-        <div class="character-card">
-            <h4 class="name">${name}</h4>
-            <img class="avatar" src=${avatar} />
-            <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container">${diceHtml}
-        </div>
-    `;
-}
-
 //Rendering two characters
-renderCharacter(hero);
-renderCharacter(enemy);
+
+blueKnight.getCharacter();
+redKnight.getCharacter();
