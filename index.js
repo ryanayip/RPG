80b719562fe460569e276810c044fec8e6f5f329
ryip94 <ryip94@yahoo.com>
2023-08-03 23:34:10 -0700

    Map
    Removed for loop and converted to map() to DRY

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hero = {
   name: "Hero",
   avatar: "images/knight-blue.png",
   health: 60,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
 };
 
 const enemy = {
@@ -11,20 +12,26 @@ const enemy = {
   name: "Enemy",
   avatar: "images/knight-red.png",
   health: 10,
-  diceRoll: 4,
+  diceRoll: [2],
+  diceCount: 1,
 };
 
 function renderCharacter(character) {
-  document.getElementById(`${character.elementId}`).innerHTML = `
+  const { elementId, name, avatar, health, diceRoll, diceCount } = character;
+
+  let diceHtml = diceRoll.map((dice) => {
+    return `<div class="dice">${dice}</div>`;
+  });
+
+  document.getElementById(`${elementId}`).innerHTML = `
         <div class="character-card">
-            <h4 class="name">${character.name}</h4>
-            <img class="avatar" src=${character.avatar} />
-            <p class="health">health: <b> ${character.health} </b></p>
-            <div class="dice-container"><div class="dice">${character.diceRoll}</div></div>
+            <h4 class="name">${name}</h4>
+            <img class="avatar" src=${avatar} />
+            <p class="health">health: <b> ${health} </b></p>
+            <div class="dice-container">${diceHtml}
         </div>
     `;
 }
+
 renderCharacter(hero);
 renderCharacter(enemy);
-
-console.log("Testing Github desktop");
