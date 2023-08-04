8c9d718700de6ea061d90bee32b29a6dda579364
ryip94 <ryip94@yahoo.com>
2023-08-03 22:30:36 -0700

    Objects
    Refactored hard coded character variables into objects. Added renderCharacter function.

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-document.getElementById("hero").innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Knight </h4>
-        <img class="avatar" src="images/knight-blue.png" />
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container"><div class="dice"> 6 </div></div>
-    </div>   
-`;
+const hero = {
+  elementId: "hero",
+  name: "Hero",
+  avatar: "images/knight-blue.png",
+  health: 60,
+  diceRoll: 6,
+};
 
-document.getElementById("enemy").innerHTML = `
-    <div class="character-card">
-        <h4 class="name">Enemy</h4>
-        <img class="avatar" src="images/knight-red.png" />
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container"><div class="dice">4</div></div>
-    </div>
-`;
+const enemy = {
+  elementId: "enemy",
+  name: "Enemy",
+  avatar: "images/knight-red.png",
+  health: 10,
+  diceRoll: 4,
+};
+
+function renderCharacter(character) {
+  document.getElementById(`${character.elementId}`).innerHTML = `
+        <div class="character-card">
+            <h4 class="name">${character.name}</h4>
+            <img class="avatar" src=${character.avatar} />
+            <p class="health">health: <b> ${character.health} </b></p>
+            <div class="dice-container"><div class="dice">${character.diceRoll}</div></div>
+        </div>
+    `;
+}
+renderCharacter(hero);
+renderCharacter(enemy);
+
+console.log("Testing Github desktop");
